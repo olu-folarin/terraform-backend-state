9389571e55ad5fa740d594f56a30dba751d75c81
olufolarin <folami25@gmail.com>
2022-02-22 14:27:48 +0100

    adding other features to the bucket

--- main.tf
+++ main.tf
@@ -13,7 +13,26 @@ provider "aws" {
 
 // create an s3 bucket where the states of different projecta can be stored with different ids
 provider "aws_s3_bucket" "backend_bucket" {
-    bucket = "dev-applications-backend-state"
+    bucket = "dev-applications-backend-state-olufolarin"
+
+    // add lifecycle to prevent the deletion of the bucket
+    lifecycle {
+        prevent_destroy = true
+    }
+
+    // enable versioning in order to store multiple versions of the state
+    versioning {
+        enabled = true
+    }
+
+    // encrypt then store the bucket in a server
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
 }
 
 // lock the state with Dynamo DB to prevent any team member from tampering from it
